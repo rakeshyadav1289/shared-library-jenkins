ae41c88fbcbf09c7dc100d3c9dd99dedbf758bfe
rakeshyadav1289 <129735141+rakeshyadav1289@users.noreply.github.com>
2026-04-03 13:12:17 +0530

    Fix formatting in git command for cloning

--- vars/clone.groovy
+++ vars/clone.groovy
@@ -1,6 +1,6 @@
 def call(string branch, string  url)
 {
   echo "git clonning"
-  git branch: "${branch}" , url: "${url}"
+  git branch: "${branch}", url: "${url}"
   echo 'git cloning successfull'
 }
